017859584e25a6534cdf3d52f32d1f396dff4044
Yongwei Dou <yongwei.dou@gmail.com>
2014-03-31 16:15:19 +0900

    Initial Commit

---
+++ DOPhotoViewer/DOPhotoViewer.h
@@ -0,0 +1,13 @@
+//
+//  DOPhotoViewer.h
+//  DOPhotoViewer
+//
+//  Created by Yongwei Dou on 2014/03/31.
+//  Copyright (c) 2014年 DODOPIPE LIMITED. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DOPhotoViewer : NSObject
+
+@end
---
+++ DOPhotoViewer/DOPhotoViewer.m
@@ -0,0 +1,13 @@
+//
+//  DOPhotoViewer.m
+//  DOPhotoViewer
+//
+//  Created by Yongwei Dou on 2014/03/31.
+//  Copyright (c) 2014年 DODOPIPE LIMITED. All rights reserved.
+//
+
+#import "DOPhotoViewer.h"
+
+@implementation DOPhotoViewer
+
+@end
